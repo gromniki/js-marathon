3750f3a3037396a78c70d255ddfdeca57efb9f18
Roman Golubev <gromniki@gmail.com>
2020-09-24 14:15:06 +0300

    Homework-2. Exercise 1 with bonus done

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const firstRow = prompt('Введите первую строку');
+const secondRow = prompt('Введите вторую строку');
+const char = prompt('По какой букве будем считать?');
+
+function countLetters(row, char) {
+  let counter = 0;
+
+  for (let i = 0; i < row.length; i++) {
+    if (row.charAt(i) === char || row.charAt(i) === 'a' || row.charAt(i) === 'A') {
+      counter += 1;
+    }
+  }
+
+  return counter;
+}
+
+function getRow(firstRow, secondRow) {
+  let numberLettersFirstRow = countLetters(firstRow, char);
+  let numberLettersSecondRow = countLetters(secondRow, char);
+
+  if (numberLettersFirstRow > numberLettersSecondRow) {
+    return firstRow;
+  } else if (numberLettersFirstRow === numberLettersSecondRow) {
+    return 'Одинаковое количество букв в двух строках';
+  } else {
+    return secondRow;
+  }
+}
+
+alert(getRow(firstRow, secondRow));
