bb8f59f0a55a3bb649d0c33641593f1872f671d7
Roman Golubev <gromniki@gmail.com>
2020-09-25 17:38:33 +0300

    Homework-3, v1

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-'use strict';
+const btn = document.querySelector('#btn-kick');
+
+const character = {
+  name: 'Pokemon',
+  defaultHP: 100,
+  damageHP: 100,
+}
+
+const enemy = {
+  name: 'Charmander',
+  defaultHP: 100,
+  damageHP: 100,
+}
+
+btn.addEventListener('click', function () {
+  console.log('Kick');
+});
