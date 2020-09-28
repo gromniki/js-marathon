cb14259e3e4319113d0fc8396148093839cae6b0
Roman Golubev <gromniki@gmail.com>
2020-09-28 18:06:35 +0300

    Homework-4, basic

--- main.js
+++ main.js
@@ -4,17 +4,21 @@ const btnKickEnemy = document.querySelector('#btn-kick-enemy');
 const character = {
   name: 'Pikachu',
   defaultHP: 100,
-  damageHP: 100,
+  damageHP: 80,
   elHP: document.querySelector('#health-character'),
-  elProgressBar: document.querySelector('#progressbar-character')
+  elProgressBar: document.querySelector('#progressbar-character'),
+  renderHPLife: renderHPLife,
+  renderProgressBarHP: renderProgressBarHP,
 }
 
 const enemy = {
   name: 'Charmander',
   defaultHP: 100,
-  damageHP: 100,
+  damageHP: 90,
   elHP: document.querySelector('#health-enemy'),
-  elProgressBar: document.querySelector('#progressbar-enemy')
+  elProgressBar: document.querySelector('#progressbar-enemy'),
+  renderHPLife: renderHPLife,
+  renderProgressBarHP: renderProgressBarHP,
 }
 
 function init() {
@@ -22,41 +26,75 @@ function init() {
   
   btn.addEventListener('click', function () {
     console.log('Kick Pikachu');
-    changeHP(random(20), character);
+    changeHP(random(20));
   });
 
   btnKickEnemy.addEventListener('click', function () {
     console.log('Kick Enemy');
-    changeHP(random(20), enemy);
+    changeHP(random(20));
   });
 }
 
 init();
 
-function renderHP(person) {
-  renderHPLife(person);
-  renderProgressBarHP(person);
+function renderHP() {
+  //renderHPLife(this);
+  //renderProgressBarHP(person);
 }
 
-function renderHPLife(person) {
-  person.elHP.textContent = person.damageHP + ' / ' + person.defaultHP;
+function renderHPLife() {
+  this.elHP.textContent = this.damageHP + ' / ' + this.defaultHP;
 }
 
-function renderProgressBarHP(person) {
-  person.elProgressBar.style.width = person.damageHP + '%';
+
+
+function renderProgressBarHP() {
+  this.elProgressBar.style.width = this.damageHP + '%';
 }
 
-function changeHP(count, person) {
-  if (person.damageHP < count) {
-    person.damageHP = 0;
-    alert('Бедный ' + person.name + ' потерпел поражение!');
+
+
+function changeHP(count) {
+  if (this.damageHP < count) {
+    this.damageHP = 0;
+    alert('Бедный ' + this.name + ' потерпел поражение!');
     btn.disabled = true;
   } else {
-    person.damageHP -= count;
+    this.damageHP -= count;
   }
 
-  renderHP(person);
+  character.renderHPLife();
+  enemy.renderHPLife();
+  character.renderProgressBarHP();
+  enemy.renderProgressBarHP();
 }
+
+
+
+// function renderHP(person) {
+//   renderHPLife(person);
+//   renderProgressBarHP(person);
+// }
+
+// function renderHPLife(person) {
+//   person.elHP.textContent = person.damageHP + ' / ' + person.defaultHP;
+// }
+
+// function renderProgressBarHP(person) {
+//   person.elProgressBar.style.width = person.damageHP + '%';
+// }
+
+// function changeHP(count, person) {
+//   if (person.damageHP < count) {
+//     person.damageHP = 0;
+//     alert('Бедный ' + person.name + ' потерпел поражение!');
+//     btn.disabled = true;
+//   } else {
+//     person.damageHP -= count;
+//   }
+
+//   renderHP(person);
+// }
 
 function random(num) {
   return Math.ceil(Math.random() * num);
