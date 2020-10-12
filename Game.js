009670c14485584b90e157bb33c45c4ceaadb5a1
Roman Golubev <gromniki@gmail.com>
2020-10-12 18:03:23 +0300

    homework-9, v07

--- Game.js
+++ Game.js
@@ -6,7 +6,6 @@ class Game {
   getPokemons = async () => {
     const responce = await fetch('https://reactmarathon-api.netlify.app/api/pokemons');
     const body = await responce.json();
-    //console.log(body);
     return body;
   }
 
--- main.js
+++ main.js
@@ -1,8 +1,4 @@
 import Game from './Game.js';
-import Fight from './Fight.js';
 
 const game = new Game();
 game.startGame();
-
-// const fight = new Fight();
-// fight.randomKicks();
